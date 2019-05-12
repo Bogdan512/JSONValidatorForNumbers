180e528a1c9ebaa38dcdfba3a66c8fc6bb95c8a5
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-05-12 08:32:53 +0300

    First Red Test Where The Method Is Not Implemented

---
+++ JsonValidatorForNumbers/JsonValidatorForNumbers.Test/ProgramTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace JsonValidatorForNumbers.Test
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void FirstTestWhereTheMethodIsNotImplemented()
+        {
+            Program.ValidateJSONNumber();
+        }
+    }
+}
---
+++ JsonValidatorForNumbers/JsonValidatorForNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JsonValidatorForNumbers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Read();
+        }
+
+        public static bool ValidateJSONNumber()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
